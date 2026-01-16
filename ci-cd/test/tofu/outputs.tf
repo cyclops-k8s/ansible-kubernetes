565dde139c8655d817b00ce8b4015394a4e72952
Ed Cooke <edward@frakkingsweet.com>
2026-01-16 23:56:41 +0000

    Add logging for ci-cd tofu and store password in secret

--- ci-cd/test/tofu/outputs.tf
+++ ci-cd/test/tofu/outputs.tf
@@ -9,26 +9,30 @@ output "information" {
         proxies        = ansible_group.proxies
         kubernetes     = ansible_group.kubernetes
       }
-      # hosts = {
-      #   control_planes = ansible_host.control-planes
-      #   proxy          = ansible_host.proxy
-      #   worker_nodes   = ansible_host.worker-nodes
-      # }
+      hosts = {
+        control_planes = ansible_host.control-planes
+        proxy          = ansible_host.proxy
+        worker_nodes   = ansible_host.worker-nodes
+      }
     }
     control_planes = module.vm-controlplanes
-    # data = {
-    #   control_planes = data.kubernetes_resource.control_planes.*.object
-    #   proxy          = data.kubernetes_resource.proxy.object
-    #   workers        = data.kubernetes_resource.workers.*.object
-    # }
+    data = {
+      control_planes = data.kubernetes_resource.control_planes.*.object
+      proxy          = data.kubernetes_resource.proxy.object
+      workers        = data.kubernetes_resource.workers.*.object
+    }
     locals = {
       kubernetes_config = local.kubernetes_config
       special_config    = local.special_config
     }
+    machine_names = {
+      control_planes = [for cp in module.vm-controlplanes : cp.hostname]
+      proxy          = module.vm-proxy.hostname
+      workers        = [for w in module.vm-workers : w.hostname]
+    }
     proxy       = module.vm-proxy
     vm_password = random_password.vm-password.result
     workers     = module.vm-workers
-
   }
   sensitive = true
 }
--- ci-cd/test/tofu/virtual-machine/cloud-init-secret.tf
+++ ci-cd/test/tofu/virtual-machine/cloud-init-secret.tf
@@ -4,15 +4,16 @@ resource "kubernetes_secret_v1" "cloud-init" {
     namespace = var.namespace_name
   }
   data = {
-    "networkdata" = templatefile(var.networkdata_filename, {
+    networkdata = templatefile(var.networkdata_filename, {
       hostname       = var.hostname
       authorized_key = var.authorized_key
       password       = var.password
     })
-    "userdata" = templatefile(var.userdata_filename, {
+    userdata = templatefile(var.userdata_filename, {
       hostname       = var.hostname
       authorized_key = var.authorized_key
       password       = var.password
     })
+    vm_password = var.password_plain
   }
 }
