f5afd7accdc108b34811d7c2729cb67953eb434f
Ed Cooke <edward@frakkingsweet.com>
2026-01-16 03:32:15 +0000

    Reduce cpu usage

--- ci-cd/test/tofu/ansible-stuff.tf
+++ ci-cd/test/tofu/ansible-stuff.tf
@@ -15,27 +15,27 @@ resource "ansible_group" "kubernetes" {
   variables = local.kubernetes_config
 }
 
-# resource "ansible_host" "control-planes" {
-#   count  = 3
-#   name   = "${kubernetes_manifest.base_data_volume.object.metadata.name}-cp${count.index + 1}.${var.namespace_name}"
-#   groups = ["control_planes", "kubernetes"]
-#   variables = {
-#     ansible_host               = module.vm-controlplanes[count.index].hostname
-#     ip_address                 = data.kubernetes_resource.control_planes[count.index].object.status.interfaces[0].ipAddress
-#     kubernetes_kubelet_node_ip = data.kubernetes_resource.control_planes[count.index].object.status.interfaces[0].ipAddress
-#   }
-# }
+resource "ansible_host" "control-planes" {
+  count  = 3
+  name   = "${kubernetes_manifest.base_data_volume.object.metadata.name}-cp${count.index + 1}.${var.namespace_name}"
+  groups = ["control_planes", "kubernetes"]
+  variables = {
+    ansible_host               = module.vm-controlplanes[count.index].hostname
+    ip_address                 = data.kubernetes_resource.control_planes[count.index].object.status.interfaces[0].ipAddress
+    kubernetes_kubelet_node_ip = data.kubernetes_resource.control_planes[count.index].object.status.interfaces[0].ipAddress
+  }
+}
 
-# resource "ansible_host" "worker-nodes" {
-#   count  = 2
-#   name   = "${kubernetes_manifest.base_data_volume.object.metadata.name}-w${count.index + 1}.${var.namespace_name}"
-#   groups = ["worker_nodes", "kubernetes"]
-#   variables = {
-#     ansible_host               = module.vm-workers[count.index].hostname
-#     ip_address                 = data.kubernetes_resource.workers[count.index].object.status.interfaces[0].ipAddress
-#     kubernetes_kubelet_node_ip = data.kubernetes_resource.workers[count.index].object.status.interfaces[0].ipAddress
-#   }
-# }
+resource "ansible_host" "worker-nodes" {
+  count  = 2
+  name   = "${kubernetes_manifest.base_data_volume.object.metadata.name}-w${count.index + 1}.${var.namespace_name}"
+  groups = ["worker_nodes", "kubernetes"]
+  variables = {
+    ansible_host               = module.vm-workers[count.index].hostname
+    ip_address                 = data.kubernetes_resource.workers[count.index].object.status.interfaces[0].ipAddress
+    kubernetes_kubelet_node_ip = data.kubernetes_resource.workers[count.index].object.status.interfaces[0].ipAddress
+  }
+}
 
 resource "ansible_host" "proxy" {
   name   = module.vm-proxy.hostname
--- ci-cd/test/tofu/machines.tf
+++ ci-cd/test/tofu/machines.tf
@@ -5,7 +5,7 @@ module "vm-proxy" {
   password              = bcrypt(random_password.vm-password.result)
   authorized_key        = tls_private_key.vm-ssh-key.public_key_openssh
   cpu_limit             = "2"
-  cpu_request           = "1"
+  cpu_request           = "100m"
   disk_size             = "20Gi"
   memory_size           = "2Gi"
   memory_size_request   = "1Gi"
@@ -22,7 +22,7 @@ module "vm-controlplanes" {
     password = bcrypt(random_password.vm-password.result)
     authorized_key = tls_private_key.vm-ssh-key.public_key_openssh
     cpu_limit = "2"
-    cpu_request = "1"
+    cpu_request = "100m"
     disk_size = "30Gi"
     memory_size = "2Gi"
     memory_size_request = "1Gi"
@@ -39,7 +39,7 @@ module "vm-workers" {
     password = bcrypt(random_password.vm-password.result)
     authorized_key = tls_private_key.vm-ssh-key.public_key_openssh
     cpu_limit = "2"
-    cpu_request = "1"
+    cpu_request = "100m"
     disk_size = "30Gi"
     memory_size = "2Gi"
     memory_size_request = "1Gi"
@@ -48,25 +48,25 @@ module "vm-workers" {
     userdata_filename = "./cloud-init/user-data.tpl"
 }
 
-# data "kubernetes_resource" "control_planes" {
-#     count = 3
-#     kind = "VirtualMachineInstance"
-#     api_version = "kubevirt.io/v1"
-#     metadata {
-#         name = module.vm-controlplanes[count.index].virtual-machine.metadata.name
-#         namespace = var.namespace_name
-#     }
-# }
+data "kubernetes_resource" "control_planes" {
+    count = 3
+    kind = "VirtualMachineInstance"
+    api_version = "kubevirt.io/v1"
+    metadata {
+        name = module.vm-controlplanes[count.index].virtual-machine.metadata.name
+        namespace = var.namespace_name
+    }
+}
 
-# data "kubernetes_resource" "workers" {
-#     count = 3
-#     kind = "VirtualMachineInstance"
-#     api_version = "kubevirt.io/v1"
-#     metadata {
-#         name = module.vm-workers[count.index].virtual-machine.metadata.name
-#         namespace = var.namespace_name
-#     }
-# }
+data "kubernetes_resource" "workers" {
+    count = 3
+    kind = "VirtualMachineInstance"
+    api_version = "kubevirt.io/v1"
+    metadata {
+        name = module.vm-workers[count.index].virtual-machine.metadata.name
+        namespace = var.namespace_name
+    }
+}
 
 data "kubernetes_resource" "proxy" {
     kind = "VirtualMachineInstance"
