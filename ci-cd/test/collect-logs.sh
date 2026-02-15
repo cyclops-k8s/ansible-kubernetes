056b33c8c74aab1fc0351b639dbc0c9578e3dcf3
Ed Cooke <edward@frakkingsweet.com>
2026-02-15 19:57:17 +0000

    Use 2 spaces to follow our standards

--- ci-cd/test/collect-logs.sh
+++ ci-cd/test/collect-logs.sh
@@ -13,11 +13,11 @@ if [ -n "${TOFU_OUTPUT}" ]; then
 ALL_HOSTS=$(echo "${TOFU_OUTPUT}" | jq -r '.information.value | (.proxy.hostname, .control_planes[].hostname, .workers[].hostname)' 2>/dev/null)
 
 for host in ${ALL_HOSTS}; do
-    echo "Collecting logs from ${host}..."
-    ssh "ansible@${host}.cyclops-vms" -i "${HOME}/.ssh/gh-${GITHUB_RUN_NUMBER}.pem" "sudo journalctl -u kubelet -n 1000" > "${LOG_ROOT}/${host}-kubelet.log" 2>&1 || true
-    ssh "ansible@${host}.cyclops-vms" -i "${HOME}/.ssh/gh-${GITHUB_RUN_NUMBER}.pem" "sudo journalctl -u containerd -n 500" > "${LOG_ROOT}/${host}-containerd.log" 2>&1 || true
-    ssh "ansible@${host}.cyclops-vms" -i "${HOME}/.ssh/gh-${GITHUB_RUN_NUMBER}.pem" "sudo kubeadm version" > "${LOG_ROOT}/${host}-kubeadm-version.log" 2>&1 || true
-    ssh "ansible@${host}.cyclops-vms" -i "${HOME}/.ssh/gh-${GITHUB_RUN_NUMBER}.pem" "sudo tar -cv /var/log" > "${LOG_ROOT}/${host}-log.tar" 2>/dev/null || true
+  echo "Collecting logs from ${host}..."
+  ssh "ansible@${host}.cyclops-vms" -i "${HOME}/.ssh/gh-${GITHUB_RUN_NUMBER}.pem" "sudo journalctl -u kubelet -n 1000" > "${LOG_ROOT}/${host}-kubelet.log" 2>&1 || true
+  ssh "ansible@${host}.cyclops-vms" -i "${HOME}/.ssh/gh-${GITHUB_RUN_NUMBER}.pem" "sudo journalctl -u containerd -n 500" > "${LOG_ROOT}/${host}-containerd.log" 2>&1 || true
+  ssh "ansible@${host}.cyclops-vms" -i "${HOME}/.ssh/gh-${GITHUB_RUN_NUMBER}.pem" "sudo kubeadm version" > "${LOG_ROOT}/${host}-kubeadm-version.log" 2>&1 || true
+  ssh "ansible@${host}.cyclops-vms" -i "${HOME}/.ssh/gh-${GITHUB_RUN_NUMBER}.pem" "sudo tar -cv /var/log" > "${LOG_ROOT}/${host}-log.tar" 2>/dev/null || true
 done
 fi
 
