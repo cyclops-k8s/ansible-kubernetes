1634903d6398ad16c30f636d1835761c4c2f59e2
Ed Cooke <edward@frakkingsweet.com>
2026-02-15 02:20:37 +0000

    Fix formatting

--- ci-cd/test/tofu/machines.tf
+++ ci-cd/test/tofu/machines.tf
@@ -64,7 +64,7 @@ data "kubernetes_resource" "control_planes" {
 
 data "kubernetes_resource" "workers" {
   count       = 3
-  depends_on = [ module.vm-workers ]
+  depends_on = [module.vm-workers]
   kind        = "VirtualMachineInstance"
   api_version = "kubevirt.io/v1"
   metadata {
@@ -74,7 +74,7 @@ data "kubernetes_resource" "workers" {
 }
 
 data "kubernetes_resource" "proxy" {
-  depends_on = [ module.vm-proxy ]
+  depends_on = [module.vm-proxy]
   kind        = "VirtualMachineInstance"
   api_version = "kubevirt.io/v1"
   metadata {
